96cd420ecf8ebb4ccbb8194b2ad73b0a00c7a639
Sagar Patel <17.sagar.patel@gmail.com>
2013-05-05 06:44:25 -0400

    Basic orbit and ejection implemented

---
+++ Assets/Scripts/PlanetObjectScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlanetObjectScript : MonoBehaviour 
+{
+	public float orbitRadius = 10.0f;
+	public float orbitSpeed = 10.0f;
+
+	float timeCounter;
+
+	public Vector3 velocity;
+	Vector3 oldPosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		renderer.material.color = Color.cyan;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if( Input.GetButton("Jump") )
+			transform.parent = null;
+
+		if(transform.parent != null) // if attached to a body
+		{	
+			timeCounter += orbitSpeed * Time.deltaTime;
+			transform.localPosition = new Vector3( orbitRadius * Mathf.Cos(timeCounter), orbitRadius * Mathf.Sin(timeCounter), 0);
+			velocity = (transform.position - oldPosition)/Time.deltaTime;
+			oldPosition = transform.position;
+		}
+		else
+		{
+			transform.position += velocity * Time.deltaTime;
+		}
+
+
+	
+	}
+
+
+}
---
+++ Assets/Scripts/SunObjectScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class SunObjectScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		renderer.material.color = Color.yellow;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
